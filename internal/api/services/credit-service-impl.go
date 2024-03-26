371a2c5c64d54b40066cbb28cb171719754c37ad
Fer9808 <alanmondragon1@gmail.com>
2024-03-26 11:54:58 -0600

    feat: optimize logic for assigments

--- internal/api/services/credit-service-impl.go
+++ internal/api/services/credit-service-impl.go
@@ -21,22 +21,42 @@ func NewCreditServiceImpl(repo persistence.AssignmentsRepositorInterface) *Credi
 }
 
 func (c CreditServiceImpl) Assign(investment int32) (int32, int32, int32, error) {
-	// Se realiza la iteración para asignar todos los posibles créditos de $700
-	for z := investment / 700; z >= 0; z-- {
-		remainingAfter700s := investment - z*700
-		// Se realiza la iteración para asignar todos los posibles créditos de $500 con el restante
-		for y := remainingAfter700s / 500; y >= 0; y-- {
-			remainingAfter500s := remainingAfter700s - y*500
-			// Se realiza la iteración para asignar todos los créditos de $300 con el restante
-			if remainingAfter500s%300 == 0 {
-				x := remainingAfter500s / 300
+	max700s := investment / 700
+	remaining := investment % 700
 
+	max500s := remaining / 500
+	remaining = remaining % 500
+
+	if remaining%300 == 0 {
+		max300s := remaining / 300
+
+		// Registrar asignación exitosa
+		assignment := models.Assignments{
+			Investment:    investment,
+			CreditType300: max300s,
+			CreditType500: max500s,
+			CreditType700: max700s,
+			Success:       true,
+		}
+
+		if err := c.repo.Add(&assignment); err != nil {
+			log.Println(err)
+		}
+
+		return max300s, max500s, max700s, nil
+	}
+
+	for ; max700s >= 0; max700s-- {
+		for try500s := int32(0); try500s <= max500s; try500s++ {
+			remainingAfter700sAnd500s := investment - max700s*700 - try500s*500
+			if remainingAfter700sAnd500s%300 == 0 {
+				try300 := remainingAfter700sAnd500s / 300
 				// Registrar asignación exitosa
 				assignment := models.Assignments{
 					Investment:    investment,
-					CreditType300: x,
-					CreditType500: y,
-					CreditType700: z,
+					CreditType300: try300,
+					CreditType500: try500s,
+					CreditType700: max700s,
 					Success:       true,
 				}
 
@@ -44,7 +64,7 @@ func (c CreditServiceImpl) Assign(investment int32) (int32, int32, int32, error)
 					log.Println(err)
 				}
 
-				return x, y, z, nil
+				return try300, try500s, max700s, nil
 			}
 		}
 	}
